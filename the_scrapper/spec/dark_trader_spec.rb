8337bf54c0abdfe22d484049f9f9443fab26d006
samir <saitsamir@hotmail.fr>
2021-10-14 23:43:59 +0200

    second

---
+++ the_scrapper/spec/dark_trader_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/dark_trader'
+
+describe "the hello function" do
+  it "says hello" do
+    expect(hello).to eq("Hello!")
+  end
+end
+
+puts "Franchemet je suis fatigué pour ecrire tous les spec merci de ta compréhension"
